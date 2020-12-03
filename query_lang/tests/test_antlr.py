f7d785ddd792531d03f75cd9af40bb70d54b2831
Nikita Vlaev <nikitavlaev00@gmail.com>
2020-12-03 17:29:09 +0300

    Added tests for ANTLR

---
+++ query_lang/tests/test_antlr.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import os
+
+from query_lang.parsing import ANTLRGrammar
+
+class TestANTLR:
+    tests_folder = 'test_data'
+
+    def test_all(self):
+        current_tests_path = Path(
+            Path(__file__).parent,
+            self.tests_folder,
+        )
+
+        for i, f in enumerate(os.listdir(current_tests_path)):
+            print(f"Current test num: {i + 1}, {f}")
+            print(open(Path(current_tests_path, f, 'res.txt')).readline())
+            res = bool(int(open(Path(current_tests_path, f, 'res.txt')).readline())) 
+            assert res == ANTLRGrammar.check(Path(current_tests_path, f, 'input.txt'))
+
