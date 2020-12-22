dfb6a0c590569a37c096208288fc2db3cf5a8bd0
Nikita Vlaev <nikitavlaev00@gmail.com>
2020-12-22 23:45:52 +0300

    Added derivative-based regular language parser

---
+++ derivative_regex_parser/regex.py
@@ -0,0 +1,62 @@
+from pyformlang.finite_automaton import Symbol
+from pyformlang.regular_expression import Regex as pfl_Regex
+from pyformlang.regular_expression.regex_objects import Empty, Epsilon, Operator, Concatenation, KleeneStar, Union
+
+class Regex(pfl_Regex):
+    def _nullable(self):
+        if str(self.head) == str(Empty()):
+            return False
+        if str(self.head) == str(Epsilon()):
+            return True # !!!!!!!!!!! Symbol(?)
+        
+        if str(self.head) == str(Concatenation()): # simple string here
+            r1 = Regex(str(self.sons[0]))
+            r2 = Regex(str(self.sons[1]))
+            return r1._nullable() and r2._nullable()
+
+        if str(self.head) == str(KleeneStar()):
+            return True
+
+        if str(self.head) == str(Union()):
+            return Regex(str(self.sons[0]))._nullable() or Regex(str(self.sons[1]))._nullable()
+
+        return False
+
+    def _dSymbol(self, c):
+        print(f"head {self.head._value}")
+        if str(self.head) == str(Empty()):
+            return self
+        if str(self.head) == str(Epsilon()):
+            return Regex(str(Empty()))
+        
+        if str(self.head) == str(Concatenation()): # simple string here
+                r1 = Regex(str(self.sons[0]))
+                r2 = Regex(str(self.sons[1]))
+                print(r1, r2)
+                if r1._nullable():
+                    return r1._dSymbol(c).concatenate(r2).union(r2._dSymbol(c))
+                else:
+                    return r1._dSymbol(c).concatenate(r2)
+
+        if str(self.head) == str(KleeneStar()):
+            return Regex(str(self.sons[0]))._dSymbol(c).concatenate(self)
+
+        if str(self.head) == str(Union()):
+            return Regex(str(self.sons[0]))._dSymbol(c).union(Regex(str(self.sons[1]))._dSymbol(c))
+
+        if self.head._value == c:
+            return Regex(Epsilon()._value)
+        else:
+            print(self.head._value, c)
+            return Regex(str(Empty()))
+        assert False
+
+    def d_accepts(self, word):
+        if len(word) > 0:
+            regex = Regex(str(self._dSymbol(word[0])))
+            print(f"first {regex}")
+            for c in word[1:]:
+                regex = Regex(str(regex._dSymbol(c)))
+            return regex._nullable()
+        else:
+            return self._nullable()
